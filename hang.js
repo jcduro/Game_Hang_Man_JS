54304e0c4764023ff36224d2cbbb28114f4b54ea
JUAN CARLOS <69373493+jcduro@users.noreply.github.com>
2025-11-08 16:44:39 -0500

    Add files via upload

---
+++ hang.js
@@ -0,0 +1,112 @@
+// ### VARIABLES ###
+
+// Array de palabras
+var palabras = [["blackpanther", "he is wild"], ["superman", "he is powerful"], ["gamora", "she is green"], ["spiderman", "like an insect"], ["wolverine", "he has claws"], ["greenlantern", "he has a jewel"], ["robin", "no powers"], ["Batman", "no powers, just money"], ["blackwidow", "she has many skills"], ["ironman", "he has a light on his chest"], ["wonderwoman", "She has a rope of truth"], ["flash", "he is really speed"], ["groot", "it is wood"], ["shazam", "just say his name"], ["cyclope", "he has a laser in his eyes"], ["Rocket", "he is like a raccoon"], ["thor", "he has an amazing hammer"], ["deadpool", "he is inmortal"], ["vision", "He is an android"], ["captainmarvel", "She is our last chance"]];
+// Palabra a averiguar
+var palabra = "";
+// NÂº aleatorio
+var rand;
+// Palabra oculta
+var oculta = [];
+// Elemento html de la palabra
+var hueco = document.getElementById("palabra");
+// Contador de intentos
+var cont = 6;
+// Botones de letras
+var buttons = document.getElementsByClassName('letra');
+// Boton de reset
+var btnInicio = document.getElementById("reset");
+
+
+// ### FUNCIONES ###
+
+// Escoger palabra al azar
+function generaPalabra() {
+  rand = (Math.random() * 20).toFixed(0);
+  palabra = palabras[rand][0].toUpperCase();
+  console.log(palabra);
+}
+
+// Funcion para pintar los guiones de la palabra
+function pintarGuiones(num) {
+  for (var i = 0; i < num; i++) {
+    oculta[i] = "_";
+  }
+  hueco.innerHTML = oculta.join("");
+}
+
+//Generar abecedario
+function generaABC (a,z) {
+  document.getElementById("abcdario").innerHTML = "";
+  var i = a.charCodeAt(0), j = z.charCodeAt(0);
+  var letra = "";
+  for( ; i<=j; i++) {
+    letra = String.fromCharCode(i).toUpperCase();
+    document.getElementById("abcdario").innerHTML += "<button value='" + letra + "' onclick='intento(\"" + letra + "\")' class='letra' id='"+letra+"'>" + letra + "</button>";
+    if(i==110) {
+      document.getElementById("abcdario").innerHTML += "<button value='Ã‘' onclick='intento(\"Ã‘\")' class='letra' id='"+letra+"'>Ã‘</button>";
+    }
+  }
+}
+
+// Chequear intento
+function intento(letra) {
+  document.getElementById(letra).disabled = true;
+  if(palabra.indexOf(letra) != -1) {
+    for(var i=0; i<palabra.length; i++) {
+      if(palabra[i]==letra) oculta[i] = letra;
+    }
+    hueco.innerHTML = oculta.join("");
+    document.getElementById("acierto").innerHTML = "Good!";
+    document.getElementById("acierto").className += "acierto verde";
+  }else{
+    cont--;
+    document.getElementById("intentos").innerHTML = cont;
+    document.getElementById("acierto").innerHTML = "Wrong!";
+    document.getElementById("acierto").className += "acierto rojo";
+    document.getElementById("image"+cont).className += "fade-in";
+  }
+  compruebaFin();
+  setTimeout(function () {
+    document.getElementById("acierto").className = "";
+  }, 800);
+}
+
+// Obtener pista
+function pista() {
+  document.getElementById("hueco-pista").innerHTML = palabras[rand][1];
+}
+
+// Compruba si ha finalizado
+function compruebaFin() {
+  if( oculta.indexOf("_") == -1 ) {
+    document.getElementById("msg-final").innerHTML = "Congratulations !!";
+    document.getElementById("msg-final").className += "zoom-in";
+    document.getElementById("palabra").className += " encuadre";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function() { location.reload() };
+  }else if( cont == 0 ) {
+    document.getElementById("msg-final").innerHTML = "Game Over";
+    document.getElementById("msg-final").className += "zoom-in";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function () { location.reload() };
+  }
+}
+
+// Restablecer juego
+function inicio() {
+  generaPalabra();
+  pintarGuiones(palabra.length);
+  generaABC("a","z");
+  cont = 6;
+  document.getElementById("intentos").innerHTML=cont;
+}
+
+// Iniciar
+window.onload = inicio();
